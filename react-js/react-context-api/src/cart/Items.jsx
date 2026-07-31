a41e9c7267405d8a9a5dcc80f27414096a75468b
Abu Huzaifa <abuhuzaifaw7@gmail.com>
2026-08-01 00:54:47 +0530

    update changes

--- react-js/react-context-api/src/cart/Items.jsx
+++ react-js/react-context-api/src/cart/Items.jsx
@@ -9,17 +9,14 @@ const Items = (props) => {
       <div className="bg-gray-200 p-5 rounded-sm border border-gray-300">
         <h1>{props.name}</h1>
         <p>Price : $ {props.price}</p>
-        
-        
         <button onClick={() => cart.setItem([...cart.item, {name:props.name, price:props.price}])}
-        
         className="mt-3 mr-4 bg-[#202002] text-white p-2 text-xs rounded-xs border border-[#404040] cursor-pointer">
           Add To Cart
         </button>
 
         <button
           onClick={() =>
-            cart.setItem(cart.item.filter((item) => item.id !== props.id))
+            cart.setItem(cart.item.filter((item) => item.name !== props.name))
           }
           className="mt-3 mx-2 bg-[#202002] text-white p-2 text-xs rounded-xs border border-[#404040] cursor-pointer"
         >
